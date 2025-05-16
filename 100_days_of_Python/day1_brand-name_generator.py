ec90173fecfa4e55ee3ac9497becae60739f35ea
Charles <mulingacharles@gmail.com>
2025-05-16 11:33:35 +0300

    Initial commit: Added 100 days of Python project upto day 4

---
+++ 100_days_of_Python/day1_brand-name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator\n")
+city = input("Which city did you grow up in\n?")
+pet = input("What is the name of your pet?\n")
+print("Your suggested band name is: "+city+" "+pet)
+
---
+++ training3_conditional_flow.py
@@ -0,0 +1,27 @@
+#How to use if then else
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L\n")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N:\n ")
+extra_cheese = input("Do you want extra cheese? Y or N:\n ")
+
+bill = 0
+
+if size == "S":
+    bill+=15
+elif size =="M":
+    bill+=20
+elif size =="L":
+    bill+=25
+else:
+    print("You typed the wrong inputs")
+
+if pepperoni =="Y":
+    if size == "S":
+        bill+=2
+    else:
+        bill+=3
+
+if extra_cheese == "Y":
+    bill+=1
+
+print(f"Your final bill is : ${bill}")
